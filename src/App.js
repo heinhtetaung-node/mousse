eb8d4df7be1cdab460d6421030ba780f10a79f40
sithumoemyint-code <75661999+sithumoemyint-code@users.noreply.github.com>
2022-04-30 11:08:26 +0630

    Fix
    Need to fix.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Home, Category, About, Size, Navbar } from './pages/index'
 
 const App = () => {
   return (
     <>
       <Navbar/>
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/category' component={Category} />
-        <Route path='/about' component={About} />
-        <Route path='/size' component={Size} />
-      </Switch>
+      <Router>
+        <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/category' component={Category} />
+            <Route path='/about' component={About} />
+            <Route path='/size' component={Size} />
+        </Switch>
+      </Router>
     </>
   )
 }
